f9715e3170705ad61aefe22402d5ab2bc93ddfc1
Kausbj <netekakalupatai@gmail.com>
2025-08-15 22:50:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "XMVD2LZI#Vr9gFXF92uS5I3KQhxiUV0zPJNIEDi1jdJ6B91jfWMU",
+
+MONGODB: process.env.MONGODB || "mongodb://mongo:TdlccGBRitoynUsOmnyGJwfFwCVhdBbd@nozomi.proxy.rlwy.net:35163",
+};
+
